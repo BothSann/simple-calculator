907bbe4d335c2f3f30f3dd62723c2560e29249f7
BothSann <boththann76@gmail.com>
2025-04-09 16:20:20 +0700

    Add simple calculator functionality with input validation

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,26 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to Simple Calculator!");
+
+        System.out.print("Please enter the first number:");
+        double firstNumber = getValidNumber(scanner);
+
+
+    }
+
+    public static double getValidNumber(Scanner scanner) {
+        while (true) {
+            if (scanner.hasNextDouble()) {
+                return scanner.nextDouble();
+            } else {
+                System.out.print("Invalid input. Please enter a valid number:");
+                scanner.next();
+            }
+        }
     }
 }
